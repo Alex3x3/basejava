5a2c5f5127744bd82969441c4ee165518481964b
AlexP <akskif@mail.ru>
2022-10-11 15:19:44 +0300

    ArrayStorage 1-st review corrections

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,19 +1,22 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
+    private int resumeCount = 0;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, resumeCount, null);
+        resumeCount = 0;
     }
 
     void save(Resume r) {
-        int resumeNum = size();
-        if (resumeNum < storage.length) {
-            storage[resumeNum] = r;
+        if (resumeCount < storage.length) {
+            storage[resumeCount] = r;
+            resumeCount++;
         }
     }
 
@@ -27,44 +30,30 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int deletePos = -1;
-        int resumesNum = size();
-        for (int i = 0; i < resumesNum; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                deletePos = i;
-                break;
+        boolean isFound = false;
+        for (int i = 0; i < resumeCount; i++) {
+            if (!isFound) {
+                if (storage[i].uuid.equals(uuid)) {
+                    isFound = true;
+                }
+            }
+
+            if (isFound && i < resumeCount - 1) {
+                storage[i] = storage[i + 1];
             }
         }
-
-        if (deletePos == -1) {
-            return;
-        }
-
-        for (int i = deletePos; i < resumesNum - 1; i++) {
-            storage[i] = storage[i + 1];
-        }
-        storage[resumesNum - 1] = null;
+        storage[resumeCount - 1] = null;
+        resumeCount--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int resumeNum = size();
-        Resume[] destination = new Resume[resumeNum];
-        System.arraycopy(storage, 0, destination, 0, resumeNum);
-        return destination;
+        return Arrays.copyOf(storage, resumeCount);
     }
 
     int size() {
-        int resumeNum = 0;
-        for (Resume resume : storage) {
-            if (resume == null) {
-                break;
-            }
-            resumeNum++;
-        }
-        return resumeNum;
+        return resumeCount;
     }
 }
